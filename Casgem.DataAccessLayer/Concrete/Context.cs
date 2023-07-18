8de546c85559e8cb0f242c015a136b9d3c523ebb
aleyna çelik <celikaleyna71@gmail.com>
2023-07-18 18:29:14 +0300

    Add project files.

---
+++ Casgem.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using Casgem.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-4FNRCPJ\\SQLEXPRESS;initial catalog=CasgemApiDb;integrated security=true;");
+        }
+        public DbSet<Customer> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Category> Customers { get; set; }
+    }
+}
